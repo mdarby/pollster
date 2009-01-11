5b27096f52a4e2ac71415631b5a9a38a9d099218
Matt Darby <matt@matt-darby.com>
2009-01-11 15:16:37 -0500

    Moved file

--- rails/generators/pollster/templates/helpers/polls_helper.rb
+++
@@ -1,9 +0,0 @@
-module PollsHelper
-
-  def add_option_link(poll, name)
-    link_to_function name do |page|
-      page.insert_html :bottom, :options, :partial => 'option_form', :locals => {:option => []}
-    end
-  end
-
-end
