80b61c18e137add03d1bbb0443746cc79396387c
Matt Darby <matt@matt-darby.com>
2009-01-11 15:15:34 -0500

    Now copying files to correct locations

--- rails/generators/pollster/pollster_generator.rb
+++ rails/generators/pollster/pollster_generator.rb
@@ -7,13 +7,16 @@ class PollsterGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
-      # m.route_resources "polls"
+      timestamp = Time.now.strftime("%Y%m%d%H%M%S")
       
-      # Controller, helper, views, and spec directories.
+      map_routes(m)
+      
       m.directory(File.join('app/models'))
       m.directory(File.join('app/controllers'))
       m.directory(File.join('app/helpers'))
       m.directory(File.join('app/views'))
+      m.directory(File.join('app/views/polls'))
+      m.directory(File.join('db/migrate'))
       
       if has_rspec?
         m.directory(File.join('spec/controllers'))
@@ -21,23 +24,39 @@ class PollsterGenerator < Rails::Generator::Base
         m.directory(File.join('spec/helpers'))
         m.directory(File.join('spec/views'))
       end
-    
-      m.template "#{@resource_generator}:helper.rb",
-        File.join('app/helpers', controller_class_path, "#{controller_file_name}_helper.rb")
-
-      for action in scaffold_views
-        m.template(
-          "#{@resource_generator}:view_#{action}.#{@default_file_extension}",
-          File.join('app/views', controller_class_path, controller_file_name, "#{action}.#{default_file_extension}")
-        )
+      
+      m.template "polls_controller.rb", File.join('app/controllers', "polls_controller.rb")
+      m.template "poll.rb", File.join('app/models', "poll.rb")
+      m.template "poll_vote.rb", File.join('app/models', "poll_vote.rb")
+      m.template "polls_helper.rb", File.join('app/helpers', "polls_helper.rb")
+      
+      m.template "create_pollster_tables.rb", File.join('db/migrate', "#{timestamp}_create_pollster_tables.rb")
+      
+      for view in views
+        m.template "views/#{view}.html.haml", File.join('app/views/polls', "#{view}.html.haml")
       end
       
     end
   end
   
+  def map_routes(m)
+    route_string = ":polls, :member => { :vote => :post }"
+    def route_string.to_sym; to_s; end
+    def route_string.inspect; to_s; end
+    m.route_resources route_string
+  end
+  
   def has_rspec?
     spec_dir = File.join(RAILS_ROOT, 'spec')
     options[:rspec] ||= (File.exist?(spec_dir) && File.directory?(spec_dir)) unless (options[:rspec] == false)
+  end
+  
+  def views
+    %w[ _current_results _final_results _option_form _poll _vote edit index new ]
+  end
+
+  def model_name 
+    class_name.demodulize
   end
   
   protected
