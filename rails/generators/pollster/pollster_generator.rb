55737f1058013940c50061e9935123707e53951e
Matt Darby <matt@matt-darby.com>
2009-01-11 18:03:45 -0500

    Fleshed and working with NamedBase

--- rails/generators/pollster/pollster_generator.rb
+++ rails/generators/pollster/pollster_generator.rb
@@ -1,5 +1,4 @@
-class PollsterGenerator < Rails::Generator::Base
-  attr_accessor :name, :description, :created_by_id, :created_at, :updated_at, :options, :ends_at
+class PollsterGenerator < Rails::Generator::NamedBase
   
   def initialize(*runtime_args)
     super(*runtime_args)
@@ -7,15 +6,13 @@ class PollsterGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
-      timestamp = Time.now.strftime("%Y%m%d%H%M%S")
-      
-      map_routes(m)
+      install_routes(m)
       
       m.directory(File.join('app/models'))
       m.directory(File.join('app/controllers'))
       m.directory(File.join('app/helpers'))
       m.directory(File.join('app/views'))
-      m.directory(File.join('app/views/polls'))
+      m.directory(File.join("app/views/#{table_name}"))
       m.directory(File.join('db/migrate'))
       
       if has_rspec?
@@ -25,22 +22,37 @@ class PollsterGenerator < Rails::Generator::Base
         m.directory(File.join('spec/views'))
       end
       
-      m.file "polls_controller.rb", File.join('app/controllers', "polls_controller.rb")
-      m.file "poll.rb", File.join('app/models', "poll.rb")
-      m.file "poll_vote.rb", File.join('app/models', "poll_vote.rb")
-      m.file "polls_helper.rb", File.join('app/helpers', "polls_helper.rb")
+      # Controllers
+      m.template "polls_controller.rb", File.join('app/controllers', "#{table_name}_controller.rb")
       
-      m.file "create_pollster_tables.rb", File.join('db/migrate', "#{timestamp}_create_pollster_tables.rb")
+      # Models
+      m.template "poll.rb", File.join('app/models', "#{table_name.singularize}.rb")
+      m.template "poll_vote.rb", File.join('app/models', "#{table_name.singularize}_vote.rb")
       
+      # Helpers
+      m.template "polls_helper.rb", File.join('app/helpers/', "#{table_name}_helper.rb")
+      
+      # Migrations
+      m.migration_template "create_pollster_tables.rb", "db/migrate", :migration_file_name => "create_pollster_tables"
+      
+      # Views
       for view in views
-        m.file "views/#{view}.html.haml", File.join('app/views/polls', "#{view}.html.haml")
+        m.template "views/#{view}.html.haml", File.join("app/views/#{table_name}", "#{view}.html.haml")
       end
       
     end
   end
   
-  def map_routes(m)
-    route_string = ":polls, :member => { :vote => :post }"
+  def table_name
+    class_name.tableize
+  end
+  
+  def object_name
+    table_name.singularize
+  end
+  
+  def install_routes(m)
+    route_string = ":#{table_name}, :member => { :vote => :post }"
     def route_string.to_sym; to_s; end
     def route_string.inspect; to_s; end
     m.route_resources route_string
